dedab69b2fe065556bbf0ac72a2d6f1d7c84aa06
Andrey <burak_andrej@mail.ru>
2021-11-03 22:09:55 +0300

    task-9

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { ImportController } from './import/import.controller';
 import { ProductController } from './product/product.controller';
 
 @Module({
   imports: [HttpModule],
-  controllers: [ImportController, ProductController],
+  controllers: [AppController, ImportController, ProductController],
   providers: [],
 })
 export class AppModule {}
